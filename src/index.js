e1f20e85697ef64d4819844b986bbff544bd85ec
VeranikaKaraliova <nika-11@mail.ru>
2021-03-15 20:36:59 +0300

    fead: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let strArr = str.split('')
+      let newArrPush =[]
+      let newArrShift =[]
+      for(let i=0; i<strArr.length/2; i++){
+        newArrPush.push([strArr[i], (strArr[strArr.length-i-1])])
+        newArrShift.unshift([strArr[i], (strArr[strArr.length-i-1])])
+      }
+      if( newArrPush.join('') === bracketsConfig.join('') || newArrShift.join('') === bracketsConfig.join('')){
+          return true
+      }else return false
 }
